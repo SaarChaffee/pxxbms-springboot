7c0a7781945bf56c913fd4cbb86720579d3ea6c6
SaarChaffee <1240148332@qq.com>
2022-04-30 10:33:53 +0800

    enable mapper scan

--- src/main/java/com/chaffee/PxxbmsSpringbootApplication.java
+++ src/main/java/com/chaffee/PxxbmsSpringbootApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.chaffee")
+@MapperScan( "com.chaffee.mapper" )
 public class PxxbmsSpringbootApplication {
   
   public static void main( String[] args ) {
