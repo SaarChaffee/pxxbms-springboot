defd0a09881867d06b76abe717088ff743632a57
SaarChaffee <1240148332@qq.com>
2022-04-30 09:39:45 +0800

    init repo

---
+++ src/main/java/com/chaffee/PxxbmsSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.chaffee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PxxbmsSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PxxbmsSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chaffee/PxxbmsSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chaffee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PxxbmsSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
