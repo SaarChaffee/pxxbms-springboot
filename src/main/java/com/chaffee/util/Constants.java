47ce3eaa878e32e0e20f7d0fd5b94b279158e06d
SaarChaffee <1240148332@qq.com>
2022-05-01 14:06:34 +0800

    add global constants

---
+++ src/main/java/com/chaffee/util/Constants.java
@@ -0,0 +1,13 @@
+/**
+ * @Name: pxxbms-springboot
+ * @Author: SaarChaffee
+ * @Code: UTF-8
+ * @Date: Created in 2022 2022/5/1
+ */
+package com.chaffee.util;
+
+public class Constants {
+  public final static String USER_SESSION = "userSession";
+  
+  public final static String MSG = "message";
+}
