578e0cffd0fdcd8a62fb6157d5cfdee9ec3ab4b3
SaarChaffee <1240148332@qq.com>
2022-05-01 15:03:12 +0800

    add mvc config

---
+++ src/main/java/com/chaffee/config/PXXMvcConfig.java
@@ -0,0 +1,29 @@
+/**
+ * @Name: pxxbms-springboot
+ * @Author: SaarChaffee
+ * @Code: UTF-8
+ * @Date: Created in 2022 2022/5/1
+ */
+package com.chaffee.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class PXXMvcConfig implements WebMvcConfigurer {
+  
+  @Override
+  public void addViewControllers( ViewControllerRegistry registry ) {
+    registry.addViewController( "/" ).setViewName( "index" );
+    registry.addViewController( "/index.html" ).setViewName( "index" );
+    registry.addViewController( "/main.html" ).setViewName( "dashboard" );
+  }
+  
+  @Bean
+  public LocaleResolver localeResolver() {
+    return new PXXLocaleResolver();
+  }
+}
