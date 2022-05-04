723b22c27b59e5d53bb84ff7f643f43f621dd9e1
SaarChaffee <1240148332@qq.com>
2022-05-04 12:19:24 +0800

    return goodType:int

--- src/main/java/com/chaffee/controller/GoodController.java
+++ src/main/java/com/chaffee/controller/GoodController.java
@@ -6,8 +6,50 @@
  */
 package com.chaffee.controller;
 
+import com.alibaba.druid.util.StringUtils;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.chaffee.entity.pojo.GoodType;
+import com.chaffee.entity.vo.GoodVO;
+import com.chaffee.service.GoodService;
+import com.chaffee.service.GoodTypeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
+@RequestMapping( "/good" )
 public class GoodController {
+  @Autowired
+  GoodService goodService;
+  @Autowired
+  GoodTypeService goodTypeService;
+  
+  @GetMapping( "/list" )
+  public String list( @RequestParam( value = "queryGoodName", required = false ) String queryGoodName,
+                      @RequestParam( value = "queryOwnerName", required = false ) String queryOwnerName,
+                      @RequestParam( value = "queryGoodType", required = false ) String queryGoodType,
+                      @RequestParam( value = "pageIndex", required = false ) String pageIndex,
+                      Model model ) {
+    queryGoodName = StringUtils.isEmpty( queryGoodName ) ? "" : queryGoodName;
+    queryOwnerName = StringUtils.isEmpty( queryOwnerName ) ? "" : queryOwnerName;
+    int goodType = StringUtils.isNumber( queryGoodType ) && !queryGoodType.contains( "-" ) ?
+        Integer.parseInt( queryGoodType ) : 0;
+    int index = StringUtils.isNumber( pageIndex ) && !pageIndex.contains( "-" ) ? Integer.parseInt( pageIndex ) : 1;
+    
+    Page<GoodVO> page = new Page<>( index, 15 );
+    List<GoodVO> goodList = goodService.queryGoodList( page, queryGoodName, queryOwnerName, goodType );
+    List<GoodType> typeList = goodTypeService.list();
+    model.addAttribute( "queryGoodName", queryGoodName );
+    model.addAttribute( "queryOwnerName", queryOwnerName );
+    model.addAttribute( "queryGoodType", goodType );
+    model.addAttribute( "typeList", typeList );
+    model.addAttribute( "pageParam", page );
+    model.addAttribute( "goods", goodList );
+    return "good/list";
+  }
 }
