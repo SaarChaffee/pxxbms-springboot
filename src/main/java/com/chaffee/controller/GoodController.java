22c175d4ad0aec9b420cf58ecfee6bfc734a45ce
SaarChaffee <1240148332@qq.com>
2022-05-04 11:00:38 +0800

    init good controller

---
+++ src/main/java/com/chaffee/controller/GoodController.java
@@ -0,0 +1,13 @@
+/**
+ * @Name: pxxbms-springboot
+ * @Author: SaarChaffee
+ * @Code: UTF-8
+ * @Date: Created in 2022 2022/5/4
+ */
+package com.chaffee.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class GoodController {
+}
