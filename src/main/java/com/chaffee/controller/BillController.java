3332b78b844386fc0c51e671d5d0c7ff9913a1ff
SaarChaffee <1240148332@qq.com>
2022-05-04 20:22:38 +0800

    init bill controller

---
+++ src/main/java/com/chaffee/controller/BillController.java
@@ -0,0 +1,10 @@
+/**
+ * @Name: pxxbms-springboot
+ * @Author: SaarChaffee
+ * @Code: UTF-8
+ * @Date: Created in 2022 2022/5/4
+ */
+package com.chaffee.controller;
+
+public class BillController {
+}
