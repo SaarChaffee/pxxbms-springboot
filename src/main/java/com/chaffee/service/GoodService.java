1ccea66bddfa19789b0c5f6f3ee7ace61db42f0c
SaarChaffee <1240148332@qq.com>
2022-05-04 10:55:19 +0800

    good service

--- src/main/java/com/chaffee/service/GoodService.java
+++ src/main/java/com/chaffee/service/GoodService.java
@@ -1,13 +1,22 @@
 package com.chaffee.service;
 
-import com.chaffee.entity.pojo.Good;
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.chaffee.entity.dto.GoodCodeDTO;
+import com.chaffee.entity.pojo.Good;
+import com.chaffee.entity.vo.GoodVO;
+
+import java.util.List;
 
 /**
-* @author RGB
-* @description 针对表【good】的数据库操作Service
-* @createDate 2022-04-30 10:31:55
-*/
+ * @author RGB
+ * @description 针对表【good】的数据库操作Service
+ * @createDate 2022-04-30 10:31:55
+ */
 public interface GoodService extends IService<Good> {
-
+  List<GoodVO> queryGoodList( IPage<GoodVO> page, String goodName, String ownerName, int goodType );
+  
+  GoodVO queryGood( long id );
+  
+  GoodCodeDTO queryGoodByCode( String goodCode );
 }
--- src/main/java/com/chaffee/service/impl/GoodServiceImpl.java
+++ src/main/java/com/chaffee/service/impl/GoodServiceImpl.java
@@ -1,20 +1,39 @@
 package com.chaffee.service.impl;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.chaffee.entity.dto.GoodCodeDTO;
 import com.chaffee.entity.pojo.Good;
+import com.chaffee.entity.vo.GoodVO;
 import com.chaffee.service.GoodService;
 import com.chaffee.mapper.GoodMapper;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 /**
-* @author RGB
-* @description 针对表【good】的数据库操作Service实现
-* @createDate 2022-04-30 10:31:55
-*/
+ * @author RGB
+ * @description 针对表【good】的数据库操作Service实现
+ * @createDate 2022-04-30 10:31:55
+ */
 @Service
 public class GoodServiceImpl extends ServiceImpl<GoodMapper, Good>
-    implements GoodService{
-
+    implements GoodService {
+  
+  @Override
+  public List<GoodVO> queryGoodList( IPage<GoodVO> page, String goodName, String ownerName, int goodType ) {
+    return baseMapper.queryGoodList( page, goodName, ownerName, goodType );
+  }
+  
+  @Override
+  public GoodVO queryGood( long id ) {
+    return baseMapper.queryGood( id );
+  }
+  
+  @Override
+  public GoodCodeDTO queryGoodByCode( String goodCode ) {
+    return baseMapper.queryGoodByCode( goodCode );
+  }
 }
 
 
