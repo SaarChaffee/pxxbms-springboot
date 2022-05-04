6453461705deb86aa91b21de9a9d9b215da774bd
SaarChaffee <1240148332@qq.com>
2022-05-04 20:22:20 +0800

    query Bill

--- src/main/java/com/chaffee/service/BillService.java
+++ src/main/java/com/chaffee/service/BillService.java
@@ -1,13 +1,25 @@
 package com.chaffee.service;
 
-import com.chaffee.entity.pojo.Bill;
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.chaffee.entity.dto.BillCodeDTO;
+import com.chaffee.entity.pojo.Bill;
+import com.chaffee.entity.vo.BillVO;
+
+import java.util.List;
 
 /**
-* @author RGB
-* @description 针对表【bill】的数据库操作Service
-* @createDate 2022-04-30 10:31:55
-*/
+ * @author RGB
+ * @description 针对表【bill】的数据库操作Service
+ * @createDate 2022-04-30 10:31:55
+ */
 public interface BillService extends IService<Bill> {
-
+  List<BillVO> queryBillList( IPage<BillVO> page,
+                              String goodName,
+                              String customerName,
+                              int paymentMethod );
+  
+  BillVO queryBill( long id );
+  
+  BillCodeDTO queryGoodByCode( String billCode );
 }
--- src/main/java/com/chaffee/service/impl/BillServiceImpl.java
+++ src/main/java/com/chaffee/service/impl/BillServiceImpl.java
@@ -1,20 +1,39 @@
 package com.chaffee.service.impl;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.chaffee.entity.dto.BillCodeDTO;
 import com.chaffee.entity.pojo.Bill;
+import com.chaffee.entity.vo.BillVO;
 import com.chaffee.service.BillService;
 import com.chaffee.mapper.BillMapper;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 /**
-* @author RGB
-* @description 针对表【bill】的数据库操作Service实现
-* @createDate 2022-04-30 10:31:55
-*/
+ * @author RGB
+ * @description 针对表【bill】的数据库操作Service实现
+ * @createDate 2022-04-30 10:31:55
+ */
 @Service
 public class BillServiceImpl extends ServiceImpl<BillMapper, Bill>
-    implements BillService{
-
+    implements BillService {
+  
+  @Override
+  public List<BillVO> queryBillList( IPage<BillVO> page, String goodName, String customerName, int paymentMethod ) {
+    return baseMapper.queryBillList( page, goodName, customerName, paymentMethod );
+  }
+  
+  @Override
+  public BillVO queryBill( long id ) {
+    return baseMapper.queryBill( id );
+  }
+  
+  @Override
+  public BillCodeDTO queryGoodByCode( String billCode ) {
+    return baseMapper.queryGoodByCode( billCode );
+  }
 }
 
 
