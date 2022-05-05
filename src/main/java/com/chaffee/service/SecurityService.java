5e12da320ba362907519076f75579e8d457f7e1f
SaarChaffee <1240148332@qq.com>
2022-05-05 14:02:52 +0800

    init SecurityService

---
+++ src/main/java/com/chaffee/service/SecurityService.java
@@ -0,0 +1,13 @@
+/**
+ * @Name: pxxbms-springboot
+ * @Author: SaarChaffee
+ * @Code: UTF-8
+ * @Date: Created in 2022 2022/5/5
+ */
+package com.chaffee.service;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public interface SecurityService extends UserDetailsService {
+
+}
