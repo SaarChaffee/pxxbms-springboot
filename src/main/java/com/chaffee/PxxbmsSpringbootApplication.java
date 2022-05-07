5b89e59c27d6928791c848d94ab43f26708eb011
SaarChaffee <1240148332@qq.com>
2022-05-07 13:21:17 +0800

    enable transaction management on Mybatis-Plus config

--- src/main/java/com/chaffee/PxxbmsSpringbootApplication.java
+++ src/main/java/com/chaffee/PxxbmsSpringbootApplication.java
@@ -1,13 +1,9 @@
 package com.chaffee;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@EnableTransactionManagement
 @SpringBootApplication
-@MapperScan( "com.chaffee.mapper" )
 public class PxxbmsSpringbootApplication {
   
   public static void main( String[] args ) {
