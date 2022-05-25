97c7bfff30dbf12084252d3120c2ec18642d5c46
SaarChaffee <1240148332@qq.com>
2022-05-25 14:26:16 +0800

    add jwt utils

---
+++ src/main/java/com/chaffee/util/JwtTokenUtil.java
@@ -0,0 +1,53 @@
+/**
+ * @Name: pxxbms-springboot
+ * @Author: SaarChaffee
+ * @Code: UTF-8
+ * @Date: Created in 2022 2022/5/25
+ */
+package com.chaffee.util;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.CompressionCodecs;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.util.Date;
+
+public class JwtTokenUtil {
+  
+  
+  public String createToken( String userName ) {
+    String token = Jwts.builder()
+        .setSubject( userName )
+        .setExpiration( new Date( System.currentTimeMillis() + Constants.TokenExpiration ) )
+        .signWith( SignatureAlgorithm.HS256, Constants.TokenSignkey )
+        .compressWith( CompressionCodecs.GZIP ).compact();
+    return token;
+  }
+  
+  public String createToken( String userName, String role ) {
+    String token = Jwts.builder()
+        .setSubject( userName )
+        .claim( Constants.UserRoleKey, role )
+        .setExpiration( new Date( System.currentTimeMillis() + Constants.TokenExpiration ) )
+        .signWith( SignatureAlgorithm.HS256, Constants.TokenSignkey )
+        .compressWith( CompressionCodecs.GZIP ).compact();
+    return token;
+  }
+  
+  public static String getUserNameFromToken( String token ) {
+    String name = Jwts.parser()
+        .setSigningKey( Constants.TokenSignkey )
+        .parseClaimsJws( token )
+        .getBody().getSubject();
+    return name;
+  }
+  
+  public static String getUserRoleFromToken( String token ) {
+    Claims claims = Jwts.parser()
+        .setSigningKey( Constants.TokenSignkey )
+        .parseClaimsJws( token )
+        .getBody();
+    return claims.get( Constants.UserRoleKey ).toString();
+  }
+}
