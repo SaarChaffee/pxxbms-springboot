57b730723a3af7018f1255b225464f727ff738d7
SaarChaffee <1240148332@qq.com>
2022-05-25 16:09:32 +0800

    DB test

--- src/test/java/com/chaffee/PxxbmsSpringbootApplicationTests.java
+++ src/test/java/com/chaffee/PxxbmsSpringbootApplicationTests.java
@@ -1,13 +1,17 @@
 package com.chaffee;
 
+import com.chaffee.mapper.UserMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class PxxbmsSpringbootApplicationTests {
-  
+  @Autowired
+  UserMapper mapper;
   @Test
   void contextLoads() {
+    System.out.println( mapper.queryLogin( "admin" ) );
   }
   
 }
