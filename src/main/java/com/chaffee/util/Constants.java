71e0f06a585f53561f35570fe8a684417a83dbf6
SaarChaffee <1240148332@qq.com>
2022-05-25 14:26:33 +0800

    add jwt constants

--- src/main/java/com/chaffee/util/Constants.java
+++ src/main/java/com/chaffee/util/Constants.java
@@ -10,4 +10,8 @@ public class Constants {
   public final static String USER_SESSION = "userSession";
   
   public final static String MSG = "msg";
+  
+  public static final long TokenExpiration = 24 * 60 * 60 * 30;
+  public static final String TokenSignkey = "41873817hdukas.#";
+  public static final String UserRoleKey = "UserRole";
 }
