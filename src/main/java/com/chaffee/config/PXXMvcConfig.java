5b1ab79eed13cfaf565ed0c4549789967076a133
SaarChaffee <1240148332@qq.com>
2022-05-30 16:30:46 +0800

    fix: the id is too large and only 16 bits are displayed in JavaScript

--- src/main/java/com/chaffee/config/PXXMvcConfig.java
+++ src/main/java/com/chaffee/config/PXXMvcConfig.java
@@ -6,12 +6,19 @@
  */
 package com.chaffee.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 public class PXXMvcConfig implements WebMvcConfigurer {
@@ -26,6 +33,23 @@ public class PXXMvcConfig implements WebMvcConfigurer {
     registry.addViewController( "/" ).setViewName( "index" );
     registry.addViewController( "/index.html" ).setViewName( "index" );
     registry.addViewController( "/main.html" ).setViewName( "dashboard" );
+  }
+  
+  @Override
+  public void extendMessageConverters( List<HttpMessageConverter<?>> converters ) {
+    MappingJackson2HttpMessageConverter jackson2HttpMessageConverter = new MappingJackson2HttpMessageConverter();
+    ObjectMapper objectMapper = jackson2HttpMessageConverter.getObjectMapper();
+    
+    SimpleModule simpleModule = new SimpleModule();
+    //将Long转为string 解决id过大 js显示问题
+    simpleModule.addSerializer( Long.class, ToStringSerializer.instance );
+    simpleModule.addSerializer(Long.TYPE, ToStringSerializer.instance);
+    //simpleModule.addSerializer( Date.class,ToStringSerializer.instance );
+    objectMapper.registerModule(simpleModule);
+    
+    jackson2HttpMessageConverter.setObjectMapper(objectMapper);
+    //放到第一个
+    converters.add(0, jackson2HttpMessageConverter);
   }
   
   @Bean
