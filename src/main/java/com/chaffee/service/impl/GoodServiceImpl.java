af821ca45ec702264de143a9f9e649ff60158896
SaarChaffee <1240148332@qq.com>
2022-06-02 15:44:29 +0800

    add VO inject

--- src/main/java/com/chaffee/service/impl/GoodServiceImpl.java
+++ src/main/java/com/chaffee/service/impl/GoodServiceImpl.java
@@ -5,8 +5,11 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.chaffee.entity.dto.GoodCodeDTO;
 import com.chaffee.entity.pojo.Good;
 import com.chaffee.entity.vo.GoodVO;
+import com.chaffee.service.GoodDescriptionService;
+import com.chaffee.service.GoodDetailsService;
 import com.chaffee.service.GoodService;
 import com.chaffee.mapper.GoodMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -19,15 +22,26 @@ import java.util.List;
 @Service
 public class GoodServiceImpl extends ServiceImpl<GoodMapper, Good>
     implements GoodService {
+  @Autowired
+  GoodDetailsService goodDetailsService;
+  @Autowired
+  GoodDescriptionService goodDescriptionService;
   
   @Override
-  public List<GoodVO> queryGoodList( IPage<GoodVO> page, String goodName, String ownerName, int goodType ) {
-    return baseMapper.queryGoodList( page, goodName, ownerName, goodType );
+  public List<GoodVO> queryGoodList( IPage<GoodVO> page, String goodName, String ownerName, long goodType ) {
+    List<GoodVO> goodVOS = baseMapper.queryGoodList( page, goodName, ownerName, goodType );
+    goodVOS.forEach( g -> {
+      g.setDetail( goodDetailsService.queryOneById( g.getId() ) );
+    } );
+    return goodVOS;
   }
   
   @Override
   public GoodVO queryGood( long id ) {
-    return baseMapper.queryGood( id );
+    GoodVO vo = baseMapper.queryGood( id );
+    vo.setDetail( goodDetailsService.queryOneById( vo.getId() ) );
+    vo.setDescription( goodDescriptionService.queryOneById( vo.getId() ) );
+    return vo;
   }
   
   @Override
